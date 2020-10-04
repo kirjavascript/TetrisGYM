decfb0f938431ab1f3825c880a70c9ece37dce84
kirjavascript <snkenjoi@gmail.com>
2020-10-04 23:58:51 +0100

    added full palette control

--- gfx/nametables/game_type_menu.js
+++ gfx/nametables/game_type_menu.js
@@ -10,72 +10,37 @@ const chars = [...buffer].map(value => lookup[value] || '__NOWAYNOWAY');
 
 console.log(chars.join('').match(/.{35}/g).join('\n'));
 
-// const tiles = `
-// W0W################################
-// WWW####qwwwwwwwwwwwwwwwwwwwwwwe####
-// W#W#qwwaTETRIS GYM (PROTOTYPE)dwwe#
-// W#W#a  zxxxxxxxxxxxxxxxxxxxxxxc  d#
-// W#W#a                            d#
-// W#W#a                            d#
-// W#W#a                            d#
-// W#W#a                            d#
-// X0W#a                            d#
-// XWW#a                            d#
-// X#W#a                            d#
-// X#W#a   PLAY                     d#
-// X#W#a   T-SPINS                  d#
-// X#W#a   PARITY                   d#
-// X#W#a   SETUPS                   d#
-// X#W#a   FLOOR                    d#
-// Y0W#a   (QUICK)TAP               d#
-// YWW#a   DROUGHT                  d#
-// Y#W#a   DEBUG MODE               d#
-// Y#W#a                            d#
-// Y#W#a                            d#
-// Y#W#a                            d#
-// Y#W#a                            d#
-// Y#W#a                            d#
-// Z0W#a                            d#
-// ZWW#a                            d#
-// Z#W#a                     KIRJAVAd#
-// Z#W#zxxxxxxxxxxxxxxxxxxxxxxxxxxxxc#
-// Z#W################################
-// Z#W################################
-// Z#W▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪
-// Z#W▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪
-// `;
-
 const tiles = `
-W0Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-WWWqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-W#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X0Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-XWWqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-X#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y0Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-YWWqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Y#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Z0Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-ZWWqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Z#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Z#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Z#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-Z#Wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+W0W################################
+WWW####qwwwwwwwwwwwwwwwwwwwwwwe####
+W#W#qwwaTETRIS GYM (PROTOTYPE)dwwe#
+W#W#a  zxxxxxxxxxxxxxxxxxxxxxxc  d#
+W#W#a                            d#
+W#W#a                            d#
+W#W#a                            d#
+W#W#a                            d#
+X0W#a                            d#
+XWW#a                            d#
+X#W#a                            d#
+X#W#a   PLAY                     d#
+X#W#a   T-SPINS                  d#
+X#W#a   PARITY                   d#
+X#W#a   SETUPS                   d#
+X#W#a   FLOOR                    d#
+Y0W#a   (QUICK)TAP               d#
+YWW#a   DROUGHT                  d#
+Y#W#a   DEBUG MODE               d#
+Y#W#a                            d#
+Y#W#a                            d#
+Y#W#a                            d#
+Y#W#a                            d#
+Y#W#a                            d#
+Z0W#a                            d#
+ZWW#a                            d#
+Z#W#a                     KIRJAVAd#
+Z#W#zxxxxxxxxxxxxxxxxxxxxxxxxxxxxc#
+Z#W################################
+Z#W################################
 `;
 
 // tiles
@@ -89,20 +54,34 @@ const practise = Buffer.from(buffer);
 
 // palettes
 // DR - DL - UR - UL
-`
-AABBCCDDEEFF
-AABBCCDDEEFF
-GGHHIIJJKKLL
-GGHHIIJJKKLL
-`;
+const palTiles = p => p.trim().match(/.+\n.+$/gm)
+    .flatMap(line=>(
+        [t,b]=line.split('\n'),
+        t.trim().match(r=/../g).map((d,i)=>d+b.trim().match(r)[i])
+    ))
+    .map(d=>+('0b'+[...d].reverse().map(d=>(+d).toString(2).padStart(2,0)).join``));
+
 [
-    [1053, [
-        0b00011011, 170, 170, 170, 170, 170, 170, 0x0,
-        170, 170, 170, 170, 170, 170, 170, 170,
-        170, 170, 170, 170, 170, 170, 170, 170,
-        170, 170, 170, 170, 170, 170, 170, 170
-    ]],
-   [1088, [170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170]],
+    [1053, palTiles(`
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+    `)],
+    [1088, palTiles(`
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+    `)],
 ].forEach(([index, attributes]) => attributes.forEach((byte, i) => { practise[i+index] = byte; }));
 
 writeFileSync(__dirname + '/game_type_menu_nametable_practise.bin', practise);
