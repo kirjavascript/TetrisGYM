be7e882563173a406db4ad7f7f2df199fd3fe155
kirjavascript <snkenjoi@gmail.com>
2021-06-18 19:19:05 +0100

    updated freespace script

--- tools/freespace.js
+++ tools/freespace.js
@@ -2,19 +2,24 @@
 
 free = 0;
 qty = 0;
-[...require('fs').readFileSync(process.argv[2])].forEach((byte, i) => {
-
-    if (i >= 0x8000) return;
-
+chunks = [];
+count = (byte) => {
     if (byte === 0) {
         qty++;
     } else {
         if (qty > 0x10) {
             free += qty;
+            chunks.push(qty);
         }
         qty = 0;
     }
+};
+[...require('fs').readFileSync(process.argv[2])].forEach((byte, i) => {
+
+    if (i >= 0x8000) return;
+    count(byte);
 });
+count(0xFF);
 
 function bytes(input, places = 2) {
     const sizes = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
