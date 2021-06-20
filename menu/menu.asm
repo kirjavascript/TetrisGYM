9bf91fb7d1b86758b3a974e529c3215caefb758c
kirjavascript <snkenjoi@gmail.com>
2021-06-20 19:09:53 +0100

    scrolltext asm£

---
+++ menu/menu.asm
@@ -0,0 +1,2 @@
+
+.include "scrolltext.asm"
--- menu/scrolltext.js
+++ menu/scrolltext.js
@@ -16,16 +16,21 @@ const scrollText = `
 
 console.log(scrollText);
 
+const chars = `0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-.>!^()`;
+const toNum = char => chars.includes(char) ? chars.indexOf(char) : 0xFF;
+const toBytes = text => [...text].map(d=>'$'+toNum(d).toString(16)).join(',');
+
 const header = scrollText.map((_,i) => `\t\t.byte scrollText${i}-scrollText`).join('\n');
-const items = scrollText.map((text,i) => `scrollText${i}:\n\t\t.byte "${text}"`).join('\n');
+const items = scrollText.map((text,i) => `scrollText${i}:\t.byte ${toBytes(text)} ; ${text}`).join('\n');
 
-const template = `
 
-scrollText:
+const template = `scrollText:
     ${header}
 ${items}
 `;
 
-console.log(template);
+
+require('fs').writeFileSync(__dirname + '/scrolltext.asm', template, 'utf8');
+
 
 console.log(`bytes >> ${scrollText.length + scrollText.reduce((a, c) => a+ c.length, 0)}`)
