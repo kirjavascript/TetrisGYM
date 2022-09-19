214b8bc2e7840fe0a9b502f58b1dea14b8714028
kirjavascript <snkenjoi@gmail.com>
2022-09-19 13:11:03 +0100

    add 2sec period cycles

--- tools/cycles.lua
+++ tools/cycles.lua
@@ -1,16 +1,23 @@
 ﻿waitForVBlankTrigger = false
 lastCycles = nil
-highest = 0
-highCount = 0
+highestShort = 0
+highCountShort = 0
+highestLong = 0
+highCountLong = 0
 
 function startFrame(address, value)
     state = emu.getState()
     waitForVBlankTrigger = false
     lastCycles = state.cpu.cycleCount
-    highCount = highCount + 1
-    if highCount > 30 then
-        highCount = 0
-        highest = 0
+    highCountShort = highCountShort + 1
+    highCountLong = highCountLong + 1
+    if highCountShort > 30 then
+        highCountShort = 0
+        highestShort = 0
+    end
+    if highCountLong > 60*2 then
+        highCountLong = 0
+        highestLong = 0
     end
 end
 
@@ -23,13 +30,16 @@ function vblankCheck(address, value)
         waitForVBlankTrigger = true
         state = emu.getState()
         diff = state.cpu.cycleCount - lastCycles
-        if diff > highest then
-            highest = diff
+        if diff > highestShort then
+            highestShort = diff
         end
-        emu.drawRectangle(8, 8, 150, 24, 0x000000, true, 1)
+        if diff > highestLong then
+            highestLong = diff
+        end
+        emu.drawRectangle(8, 8, 150, 36, 0x000000, true, 1)
         emu.drawString(12, 9, "used cycles - " .. diff .. cyclePCT(diff), 0xFFFFFF, 0xFF000000, 1)
-        emu.drawString(12, 21, "highest/sec/2 - " .. highest .. cyclePCT(highest), 0xFFFFFF, 0xFF000000, 1)
-
+        emu.drawString(12, 21, "highest/sec/2 - " .. highestShort .. cyclePCT(highestShort), 0xFFFFFF, 0xFF000000, 1)
+        emu.drawString(12, 33, "highest/2sec - " .. highestLong .. cyclePCT(highestLong), 0xFFFFFF, 0xFF000000, 1)
     end
 end
 emu.addMemoryCallback(vblankCheck, emu.memCallbackType.cpuRead, 0x33)
