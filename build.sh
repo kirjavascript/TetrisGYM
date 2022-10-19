71fe78d8c5edada08d015e46f7849dc4655c2350
kirjavascript <snkenjoi@gmail.com>
2022-10-20 00:11:44 +0100

    use built in ca65

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ node gfx/nametables/build.js
 python tools/nes-util/nes_chr_encode.py gfx/title_menu_tileset.png gfx/title_menu_tileset.chr
 python tools/nes-util/nes_chr_encode.py gfx/game_tileset.png gfx/game_tileset.chr
 python tools/nes-util/nes_chr_encode.py gfx/rocket_tileset.png gfx/rocket_tileset.chr
-/usr/bin/ca65 -l tetris.lst -g tetris.asm -o tetris.o
-/usr/bin/ca65 -l tetris.lst -g main.asm -o main.o
-/usr/bin/ca65 -l tetris.lst -g tetris-ram.asm -o tetris-ram.o
-/usr/bin/ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C tetris.nes.cfg main.o tetris-ram.o tetris.o
+ca65 -l tetris.lst -g tetris.asm -o tetris.o
+ca65 -l tetris.lst -g main.asm -o main.o
+ca65 -l tetris.lst -g tetris-ram.asm -o tetris-ram.o
+ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C tetris.nes.cfg main.o tetris-ram.o tetris.o
 sha1sum tetris.nes
 sed -n '18p;19p;24,26p' < tetris.map
 ./tools/flips-linux --create clean.nes tetris.nes tetris.bps
