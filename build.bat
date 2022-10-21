55a7bfe63b8a6a347ca5ecb9843a66955d74f636
kirjavascript <snkenjoi@gmail.com>
2022-10-21 13:03:52 +0100

    create src/ directory

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 : create nametables
 
-node gfx/nametables/build.js
+node src/gfx/nametables/build.js
 
 : PNG -> CHR
 
-python tools/nes-util/nes_chr_encode.py gfx/title_menu_tileset.png gfx/title_menu_tileset.chr
-python tools/nes-util/nes_chr_encode.py gfx/game_tileset.png gfx/game_tileset.chr
-python tools/nes-util/nes_chr_encode.py gfx/rocket_tileset.png gfx/rocket_tileset.chr
+python tools/nes-util/nes_chr_encode.py src/gfx/title_menu_tileset.png src/gfx/title_menu_tileset.chr
+python tools/nes-util/nes_chr_encode.py src/gfx/game_tileset.png src/gfx/game_tileset.chr
+python tools/nes-util/nes_chr_encode.py src/gfx/rocket_tileset.png src/gfx/rocket_tileset.chr
 
 : slower but more portable JS alternative
 : npx img2chr gfx/title_menu_tileset.png gfx/title_menu_tileset.chr
@@ -15,13 +15,13 @@ python tools/nes-util/nes_chr_encode.py gfx/rocket_tileset.png gfx/rocket_tilese
 
 : build object files
 
-ca65 -l tetris.lst -g tetris.asm -o tetris.o
-ca65 -g main.asm -o main.o
-ca65 -g tetris-ram.asm -o tetris-ram.o
+ca65 -g src/tetris.asm -o tetris.o
+ca65 -l tetris.lst -g src/main.asm -o main.o
+ca65 -g src/tetris-ram.asm -o tetris-ram.o
 
 : link object files
 
-ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C tetris.nes.cfg main.o tetris-ram.o tetris.o
+ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C src/tetris.nes.cfg main.o tetris-ram.o tetris.o
 
 : create patch
 
--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 # build / compress nametables
 
-node gfx/nametables/build.js
+node src/gfx/nametables/build.js
 
 # PNG -> CHR
 
 png2chr() {
 
-    python tools/nes-util/nes_chr_encode.py gfx/title_menu_tileset.png gfx/title_menu_tileset.chr
-    python tools/nes-util/nes_chr_encode.py gfx/game_tileset.png gfx/game_tileset.chr
-    python tools/nes-util/nes_chr_encode.py gfx/rocket_tileset.png gfx/rocket_tileset.chr
+    python tools/nes-util/nes_chr_encode.py src/gfx/title_menu_tileset.png src/gfx/title_menu_tileset.chr
+    python tools/nes-util/nes_chr_encode.py src/gfx/game_tileset.png src/gfx/game_tileset.chr
+    python tools/nes-util/nes_chr_encode.py src/gfx/rocket_tileset.png src/gfx/rocket_tileset.chr
 
     # slower but more portable JS alternative
     # npx img2chr gfx/title_menu_tileset.png gfx/title_menu_tileset.chr
@@ -20,14 +20,14 @@ png2chr() {
 
 # build CHR if it doesnt already exist
 
-if [ "$(find gfx/*.chr 2>/dev/null | wc -l)" = 0 ]; then
+if [ "$(find src/gfx/*.chr 2>/dev/null | wc -l)" = 0 ]; then
     echo "building CHR for the first time"
     png2chr
 else
 
     # if it does exist check if the PNG has been modified
 
-    pngTimes=$(stat -c "%Y" gfx/*.png)
+    pngTimes=$(stat -c "%Y" src/gfx/*.png)
     scriptTime=$(stat -c "%X" "$0")
 
     for pngTime in $pngTimes; do
@@ -41,18 +41,18 @@ fi
 
 # touch this file to store the last modified / checked date
 
-touch gfx/*.png
+touch src/gfx/*.png
 touch "$0"
 
 # build object files
 
-ca65 -l tetris.lst -g tetris.asm -o tetris.o
-ca65 -g main.asm -o main.o
-ca65 -g tetris-ram.asm -o tetris-ram.o
+ca65 -g src/tetris.asm -o tetris.o
+ca65 -l tetris.lst -g src/main.asm -o main.o
+ca65 -g src/tetris-ram.asm -o tetris-ram.o
 
 # link object files
 
-ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C tetris.nes.cfg main.o tetris-ram.o tetris.o
+ld65 -m tetris.map -Ln tetris.lbl --dbgfile tetris.dbg -o tetris.nes -C src/tetris.nes.cfg main.o tetris-ram.o tetris.o
 
 # create patch
 
