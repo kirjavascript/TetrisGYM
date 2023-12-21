ba0f426fdf1099cf8f047bb12278a02039aff9b5
Richard <zohassadar@gmail.com>
2023-12-21 08:29:54 -0700

    Update build helper

--- bm.sh
+++ bm.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
+node build.js
+mv tetris.nes tetrisauto.nes
+
+node build.js -o
+mv tetris.nes tetrisautocnrom.nes
+
 node build.js -m1
 mv tetris.nes tetrismmc1.nes
+
 node build.js -m3
 mv tetris.nes tetriscnrom.nes
+
 node build.js -m4
 mv tetris.nes tetrismmc3.nes
+
 node build.js -m5
 mv tetris.nes tetrismmc5.nes
 
