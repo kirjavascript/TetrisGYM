ea5f57a88fa2a47b976e6b73d671864b06c531fe
Richard <zohassadar@gmail.com>
2024-01-04 20:05:22 -0700

    Squashed commit of the following:
    commit da3b1326cf2a2bbd49460c1256eae5c661a2c159
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Fri Jan 5 02:05:22 2024 +0000
    
        test wording
    
    commit c8e92b4dc6a170290cd85466181ecfbbd7d8f430
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Fri Jan 5 01:11:45 2024 +0000
    
        test sps
    
    commit ae0eb4624f6cd8308f3d42b27f0f0f1b3f8da5f9
    Merge: afd957f 1ca31c1
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Thu Jan 4 23:52:42 2024 +0000
    
        Merge branch 'tests'
    
    commit 1ca31c121aac31d00a45c00fbd57e8ce81f3acaa
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Thu Jan 4 23:52:25 2024 +0000
    
        add SPS tests
    
    commit 178875dd735c9f3b564decda10c25f8234d887fa
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Tue Jan 2 00:28:20 2024 +0000
    
        SPS to tests
    
    commit 91e45bc676ed6fc1b0a41843a014b6f638b1f0a3
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Sat Dec 30 17:45:19 2023 +0000
    
        read addresses from debug info
    
    commit b9a086e745a77b3df74598425c06ab5d405eaa33
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Sat Dec 30 03:42:06 2023 +0000
    
        add pushdown points test
    
    commit 79686df4c7cb7bc9824de071f5b14d64666025b0
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Sat Dec 30 03:01:52 2023 +0000
    
        basic emulator testing framework
    
    commit afd957f1f77a0ef809833d9531e6c612e2972fb6
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Fri Dec 29 22:48:11 2023 +0000
    
        readme note
    
    commit d49df68b0d275e2bef6164c0cba0660d798413f4
    Author: kirjavascript <snkenjoi@gmail.com>
    Date:   Fri Dec 29 22:39:52 2023 +0000
    
        initial test framework

---
+++ tests/src/pushdown.rs
@@ -0,0 +1,45 @@
+use crate::{labels, util};
+
+pub fn test() {
+    let mut emu = util::emulator(None);
+
+    for pushdown in 2..15 {
+        [0..1000, 24500..25500].into_iter().for_each(|range| {
+            for score in range {
+                util::set_score(&mut emu, score);
+
+                emu.registers.pc = labels::get("addPushDownPoints");
+                emu.memory.iram_raw[labels::get("holdDownPoints") as usize] = pushdown;
+
+                util::run_to_return(&mut emu, false);
+
+                let reference = pushdown_impl(pushdown, score as u16) as u32;
+
+                assert_eq!(reference, util::get_score(&mut emu) - score);
+            }
+        });
+    }
+}
+
+// reference implementation - tested against the original game
+// may seem weird - designed to be translated to assembly
+fn pushdown_impl(pushdown: u8, score: u16) -> u16 {
+    let ones = score % 10;
+    let hundredths = score % 100;
+    let mut newscore = ones as u8 + (pushdown - 1);
+    if newscore & 0xF > 9 {
+        newscore += 6;
+    }
+
+    let low = (newscore & 0xF) as u16;
+    let high = ((newscore & 0xF0) / 16 * 10) as u16;
+
+    let mut newscore = high + (hundredths - ones);
+    let nextscore = newscore + low;
+
+    if nextscore <= 100 {
+        newscore = nextscore;
+    }
+
+    newscore + (score - hundredths) - score
+}
