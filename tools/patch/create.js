a331da11cb55ff4b28c742f98ecd176e89d5a3eb
kirjavascript <snkenjoi@gmail.com>
2024-01-06 18:09:30 +0000

    add OG usage

--- tools/patch/create.js
+++ tools/patch/create.js
@@ -1,12 +1,26 @@
-const { MarcFile, createBPSFromFiles } = require('./bps.js');
+const { MarcFile, createBPSFromFiles, parseBPSFile } = require('./bps.js');
 const fs = require('fs');
+
+// https://github.com/blakesmith/rombp/blob/master/docs/bps_spec.md
 
 module.exports = function patch(original, modified, destination) {
     const patch = createBPSFromFiles(
         new MarcFile(original),
         new MarcFile(modified),
         true,
-    ).export('')._u8array;
+    ).export('');
 
-    fs.writeFileSync(destination, patch);
+    fs.writeFileSync(destination, patch._u8array);
+
+    const bps = parseBPSFile(new MarcFile('tetris.bps'));
+
+    // count patch sizes
+    let source = 0;
+    bps.actions.forEach(action => {
+        if ([0, 2].includes(action.type)) {
+            source += action.length;
+        }
+    });
+
+    return (source / bps.sourceSize) * 100 | 0;
 }
