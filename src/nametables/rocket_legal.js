b196ef34ddfa4d8ed43831ad7550fb6b30b0843c
Richard <zohassadar@gmail.com>
2024-02-03 20:28:27 -0700

    Clean up test elements

--- src/nametables/rocket_legal.js
+++ src/nametables/rocket_legal.js
@@ -3,7 +3,6 @@ const {
     drawTiles,
     flatLookup,
     drawAttrs,
-    drawRect,
 } = require('./nametables');
 
 const legal = Array.from({ length: 1024 }, () => 0xFF);
@@ -96,8 +95,6 @@ drawTiles(legal, lookup, `
 ################################
 `);
 
-drawRect(legal, 7, 5, 10, 5, 0xA6);
-
 drawAttrs(rocket, [`
     1111111111111111
     1111111111111111
@@ -121,9 +118,9 @@ drawAttrs(rocket, [`
 drawAttrs(legal, [`
     0000000000000000
     0000000000000000
-    0000011111000000
-    0000011111000000
-    0000011111000000
+    0000000000000000
+    0000000000000000
+    0000000000000000
     0000000000000000
     0000000000000000
     0000000000000000
