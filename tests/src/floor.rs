8f9ec36c1743a229ba0fa0f67470711b5e224055
kirjavascript <snkenjoi@gmail.com>
2024-02-04 15:33:53 +0000

    test floor itself

--- tests/src/floor.rs
+++ tests/src/floor.rs
@@ -2,12 +2,59 @@ use crate::{util, labels, playfield};
 
 pub fn test() {
     test_floor();
+    test_floor_linecap();
     test_floor0();
 }
 
 fn test_floor() {
-    // check rows aren't pulled from the top in linecap floor mode
+    let mut emu = util::emulator(None);
 
+    for _ in 0..3 {
+        emu.run_until_vblank();
+    }
+
+    let practise_type = labels::get("practiseType") as usize;
+    let game_mode = labels::get("gameMode") as usize;
+    let main_loop = labels::get("mainLoop");
+    let level_number = labels::get("levelNumber") as usize;
+
+    // load floor 4
+
+    emu.memory.iram_raw[practise_type] = labels::get("MODE_FLOOR") as _;
+    emu.memory.iram_raw[level_number] = 18;
+    emu.memory.iram_raw[game_mode] = 4;
+    emu.memory.iram_raw[labels::get("floorModifier") as usize] = 4;
+
+    emu.registers.pc = main_loop;
+
+    for _ in 0..10 {
+        emu.run_until_vblank();
+    }
+
+    // check floor is height 4
+
+    for i in 0..160 {
+        assert_eq!(emu.memory.iram_raw[i + labels::get("playfield") as usize], 0xEF);
+    }
+
+    for i in 160..200 {
+        assert_ne!(emu.memory.iram_raw[i + labels::get("playfield") as usize], 0xEF);
+    }
+
+    emu.memory.iram_raw[labels::get("currentPiece") as usize] = 0x12;
+    emu.memory.iram_raw[labels::get("tetriminoX") as usize] = 0x5;
+    emu.memory.iram_raw[labels::get("tetriminoY") as usize] = 0x0f;
+    emu.memory.iram_raw[labels::get("autorepeatY") as usize] = 0;
+
+    for _ in 0..20 {
+        emu.run_until_vblank();
+    }
+
+    // check flat line doesnt burn anything
+    assert_ne!(playfield::get(&mut emu, 0, 19), 0xEF);
+}
+
+fn test_floor_linecap() {
     let mut emu = util::emulator(None);
 
     for _ in 0..3 {
@@ -54,6 +101,7 @@ fn test_floor() {
         }
     }
 
+    // check rows aren't pulled from the top in linecap floor mode
     for i in 0..40 {
         assert_eq!(emu.memory.iram_raw[i + labels::get("playfield") as usize], 0xEF);
     }
@@ -65,8 +113,6 @@ fn test_floor() {
 }
 
 fn test_floor0() {
-    // check floor 0 doesnt burn lines
-
     let mut emu = util::emulator(None);
 
     for _ in 0..3 {
@@ -108,5 +154,6 @@ fn test_floor0() {
         emu.run_until_vblank();
     }
 
+    // check floor 0 doesnt burn lines
     assert_ne!(playfield::get(&mut emu, 0, 19), 0xEF);
 }
