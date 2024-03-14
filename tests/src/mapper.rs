4413fb386c298d8a0b156fafe9d6208cb195c6c7
Richard <zohassadar@gmail.com>
2024-03-14 04:55:10 -0700

    test rocket screen mapper

--- tests/src/mapper.rs
+++ tests/src/mapper.rs
@@ -1,4 +1,4 @@
-use crate::{input, labels, util, video};
+use crate::{input, labels, playfield, util, video};
 use rusticnes_core::nes::NesState;
 
 pub fn get_expected_tilesets() -> (Vec<u8>, Vec<u8>) {
@@ -8,12 +8,12 @@ pub fn get_expected_tilesets() -> (Vec<u8>, Vec<u8>) {
     let game_tileset = include_bytes!("../../src/chr/game_tileset.chr");
     let rocket_tileset = include_bytes!("../../src/chr/rocket_tileset.chr");
     let empty_tileset: Vec<u8> = vec![0; 0x1000];
-    
+
     // cnrom limited to reading 8k banks, so pair tilesets accordingly
     let mut tileset1: Vec<u8> = vec![0; 0x2000];
     tileset1[..0x1000].copy_from_slice(title_menu_tileset);
     tileset1[0x1000..].copy_from_slice(game_tileset);
-    
+
     let mut tileset2: Vec<u8> = vec![0; 0x2000];
     tileset2[..0x1000].copy_from_slice(rocket_tileset);
     tileset2[0x1000..].copy_from_slice(&empty_tileset);
@@ -64,7 +64,10 @@ pub fn test() {
     let game_mode = labels::get("gameMode") as usize;
     let main_loop = labels::get("mainLoop");
     let level_number = labels::get("levelNumber") as usize;
-    emu.memory.iram_raw[practise_type] = labels::get("MODE_TETRIS") as _;
+    let score = labels::get("score") as usize;
+    let tetris = labels::get("MODE_TETRIS") as usize;
+
+    emu.memory.iram_raw[practise_type] = tetris as u8;
     emu.memory.iram_raw[level_number] = 18;
     emu.memory.iram_raw[game_mode] = 4;
     emu.registers.pc = main_loop;
@@ -78,6 +81,7 @@ pub fn test() {
     let current_tileset = get_current_tilesets(&mut emu);
     assert_eq!(tile_select, 1);
     assert_eq!(current_tileset, tileset1);
+
     // boot in qual: tileset2, select 0
     emu.p1_input = input::SELECT;
     emu.reset();
@@ -90,10 +94,55 @@ pub fn test() {
     assert_eq!(tile_select, 0);
     assert_eq!(current_tileset, tileset2);
     emu.p1_input = 0;
-    
+
+    // rocket screen: tileset2, select 0
+    emu.memory.iram_raw[practise_type] = tetris as u8;
+    emu.memory.iram_raw[level_number] = 12;
+    emu.memory.iram_raw[game_mode] = 4;
+    emu.registers.pc = main_loop;
+
+    for _ in 0..20 {
+        emu.run_until_vblank();
+        // view.render(&mut emu);
+    }
+    emu.memory.iram_raw[score] = 0x99;
+    emu.memory.iram_raw[score+1] = 0x99;
+    emu.memory.iram_raw[score+2] = 0x99;
+    playfield::set_str(&mut emu, r##"
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########"##);
+
+    for _ in 0..400 {
+        emu.run_until_vblank();
+        view.render(&mut emu);
+    }
+
+    let tile_select = get_tile_select(&mut emu);
+    let current_tileset = get_current_tilesets(&mut emu);
+    assert_eq!(tile_select, 0);
+    assert_eq!(current_tileset, tileset2);
+
+
     //todo:
     // high score entry screen: tileset1, select 0
 
-    // rocket screen: tileset2, select 0
 
 }
