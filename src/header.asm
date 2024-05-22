cd437ac14027b38fc5f987a72d6849c4f8ecbfd0
TakuikaNinja <takuikaninja@gmail.com>
2024-05-22 20:56:56 +1200

    Update header to NES2.0

--- src/header.asm
+++ src/header.asm
@@ -1,8 +1,8 @@
 ;
-; iNES header
+; NES2.0 header
 ;
 
-; This iNES header is from Brad Smith (rainwarrior)
+; iNES header adapted from Brad Smith (rainwarrior)
 ; https://github.com/bbbradsmith/NES-ca65-example
 
 .segment "HEADER"
@@ -11,6 +11,9 @@
 
 INES_MIRROR = 0 ; 0 = horizontal mirroring, 1 = vertical mirroring (ignored in MMC1)
 INES_SRAM   = 1 ; 1 = battery backed SRAM at $6000-7FFF
+NES2_SRAM_SHIFT = INES_SRAM * 7 ; if SRAM present, set shift to 7 for (64 << 7) = 8KiB size
+NES2_REGION = 2 ; 0 = NTSC, 1 = PAL, 2 = multi-region, 3 = UA6538 ("Dendy")
+NES2_INPUT = 1 ; 1 = standard NES/FC controllers
 
 ; Override INES_MAPPER for mode 1000 (auto detect)
 .if INES_MAPPER = 1000
@@ -20,12 +23,23 @@ INES_SRAM   = 1 ; 1 = battery backed SRAM at $6000-7FFF
     _INES_MAPPER = 1 ; MMC1 for Emulator/Flashcart
     .endif
 .else
-_INES_MAPPER = INES_MAPPER ; use actual INES_MAPPER otherwise
+    _INES_MAPPER = INES_MAPPER ; use actual INES_MAPPER otherwise
+.endif
+
+; Pick the appropriate NES2_SUBMAPPER
+.if _INES_MAPPER = 1
+    NES2_SUBMAPPER = 5 ; MMC1 fixed PRG
+.elseif _INES_MAPPER = 3
+    NES2_SUBMAPPER = 2 ; CNROM bus conflicts
+.else
+    NES2_SUBMAPPER = 0 ; otherwise don't specify submapper
 .endif
 
 .byte 'N', 'E', 'S', $1A ; ID
 .byte $02 ; 16k PRG chunk count
 .byte $02 ; 8k CHR chunk count
 .byte INES_MIRROR | (INES_SRAM << 1) | ((_INES_MAPPER & $f) << 4)
-.byte (_INES_MAPPER & %11110000)
-.byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
+.byte (_INES_MAPPER & %11110000) | %00001000 ; NES2.0 header identifier
+.byte ((NES2_SUBMAPPER & $f) << 4) ; submapper
+.byte $0, (NES2_SRAM_SHIFT << 4) ; PRG MSB, SRAM shift count
+.byte $0, NES2_REGION, $0, $0, NES2_INPUT ; misc. fields, region, input device
