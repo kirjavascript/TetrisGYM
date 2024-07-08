4b8936934b70e7e574cda8e35cebd1fb417d8bb9
zohassadar <zohassadar@gmail.com>
2024-07-08 16:46:52 +0000

    Reset spawnID between SPS sequences

--- tests/src/sps.rs
+++ tests/src/sps.rs
@@ -13,6 +13,7 @@ impl SPS {
     }
 
     pub fn set_input(&mut self, seed: (u8, u8, u8)) {
+        self.emu.memory.iram_raw[labels::get("spawnID") as usize] = 0;
         self.emu.memory.iram_raw[(labels::get("set_seed_input") + 0) as usize] = seed.0;
         self.emu.memory.iram_raw[(labels::get("set_seed") + 0) as usize] = seed.0;
         self.emu.memory.iram_raw[(labels::get("set_seed_input") + 1) as usize] = seed.1;
