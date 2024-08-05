e4348223dccd976b38fadc5e8b9df553a896d819
zohassadar <zohassadar@gmail.com>
2024-08-05 15:54:43 +0000

    Read tile from nametable in tspins test

--- tests/src/tspins.rs
+++ tests/src/tspins.rs
@@ -33,6 +33,8 @@ pub fn test() {
 ##########
     "##.trim(), playfield::get_str(&emu).trim());
 
+    // check that correct tile is rendered
+    assert_eq!(emu.ppu.read_byte(&mut *emu.mapper, 0x22CC), 0x7b);
     // check pixel is actually rendered
     assert_eq!(emu.ppu.screen[offset(96, 176) as usize], 0x30);
 }
