58315cd11e305f672297d8625bcdbd6b552edbfb
zohassadar <zohassadar@gmail.com>
2025-01-26 21:18:03 +0000

    branch differently for fewer destinations
    Throws warning when single destination is defined but optimizes to jmp
    instruction anyway.
    
    For 2 to 4 destinations, uses a decrementing x register, branch
    instructions and jmp instructions to save a minimum of 7 cycles, up to 18.
    
    Rom space is saved for 1-3 destinations.  In the case of 4 destinations,
    it uses an additional 2 bytes compared to the rts method.
    
    How each option expands:
    
    1 destination: 3 cycles
        jmp a1
    
    2 destinations: maximum 11 cycles to a0
        ldx dest
        beq addr0
        jmp a1
    addr0:
        jmp a0
    
    3 destinations: maximum 15 cycles to a1
        ldx dest
        beq addr0
        dex
        beq addr1
        jmp a2
    addr1:
        jmp a1
    addr0:
        jmp a0
    
    4 destinations: maximum 19 cycles to a2
        ldx dest
        beq addr0
        dex
        beq addr1
        dex
        beq addr2
        jmp a3
    addr2:
        jmp a2
    addr1:
        jmp a1
    addr0:
        jmp a0
    
    5+ destinations: 23-26 cycles
        ldx dest
        lda hyBytes,x
        pha
        lda loBytes,x
        pha
        rts
    hiBytes:
        .byte >a0,>a1,>a2,>a3,>a4
    hiBytes:
        .byte <a0,<a1,<a2,<a3,<a4

--- src/macros.asm
+++ src/macros.asm
@@ -10,9 +10,38 @@
 .endmacro
 
 .macro branchTo dest, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15
-    ; uses each destination-1 and rts to branch
+.scope
     ; add additional arguments as needed, max a255
-    .scope
+
+    .if .blank(a1)
+        ; single destination.  3 cycles.
+        .warning "branchTo defined with single destination, converting to jmp"
+        jmp a0
+
+    .elseif .blank(a4)
+        ; 2-4 destinations use branching.  8-19 cycles
+        ldx dest
+        beq addr0
+        .ifnblank a2
+            dex
+            beq addr1
+            .ifnblank a3
+                dex
+                beq addr2
+                addr3:
+                    jmp a3
+            .endif
+             addr2:
+                jmp a2
+        .endif
+         addr1:
+             jmp a1
+         addr0:
+             jmp a0
+
+    .else
+        ; 5+ destinations, use rts branch.  23-26 cycles
+        ; uses each destination-1 and rts to branch
         ldx dest
         lda hiBytes,x
         pha
@@ -20,9 +49,10 @@
         pha
         rts
 
-    hiBytes:
-        _makeRtsTable ">", a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15
-    loBytes:
-        _makeRtsTable "<", a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15
-    .endscope
+        hiBytes:
+            _makeRtsTable ">", a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15
+        loBytes:
+            _makeRtsTable "<", a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15
+    .endif
+.endscope
 .endmacro
