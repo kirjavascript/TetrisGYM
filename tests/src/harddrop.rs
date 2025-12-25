185c91a2fd6b6872b65e5fd73cf111b1823f5b75
zohassadar <zohassadar@gmail.com>
2025-12-25 12:39:24 +0000

    save/restore cpu.tick along with pc in harddrop test

--- tests/src/harddrop.rs
+++ tests/src/harddrop.rs
@@ -209,9 +209,11 @@ fn test_harddropped_piece(emu: &mut NesState, start: &str, finish: &str, piece: 
 
     // stage ghost piece
     let temp_pc = emu.registers.pc;
+    let temp_tick = emu.cpu.tick;
     emu.registers.pc = stage_sprite;
     let stage_cycles = util::cycles_to_return(emu);
     emu.registers.pc = temp_pc;
+    emu.cpu.tick = temp_tick;
 
     util::run_n_vblanks(emu, 1);
 
