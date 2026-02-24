0ce660c9cb3477ad7cce809e7543e077626070ed
zohassadar <zohassadar@gmail.com>
2026-02-24 14:42:56 +0000

    lua script for tas testing in mesen

---
+++ tools/tas.lua
@@ -0,0 +1,38 @@
+﻿-- uses https://tasvideos.org/7625S
+local rom = emu.getRomInfo()
+local romPath, _ = string.gsub(rom.path, rom.name, "")
+local filename =  romPath .. "r57shell_archanfel-tetris-maxscore.fm2"
+
+local inputs = {}
+for line in io.lines(filename) do
+    local buttons = line:match("^|0|(........)|||$")
+    if buttons then
+        local input = {}
+        local mapped = {
+             "right",
+             "left",
+             "down",
+             "up",
+             "start",
+             "select",
+             "b",
+             "a",
+        }
+        for i = 1, 8 do
+            input[mapped[i]] = not (buttons:sub(i, i) == ".")
+        end
+        table.insert(inputs, input)
+    end
+end
+
+function applyInputs()
+    local state = emu.getState()
+    local input = inputs[state["frameCount"]+1]
+    if not input then
+        emu.breakExecution()
+    else
+        emu.setInput(input)
+    end
+end
+
+emu.addEventCallback(applyInputs, emu.eventType.inputPolled);
