7c6f5bf95a1e818714f1149837f3e4d0ef38ef04
kirjavascript <snkenjoi@gmail.com>
2026-06-03 01:47:30 +0100

    gamemodestatefix

--- src/gamemodestate/branch.asm
+++ src/gamemodestate/branch.asm
@@ -1,14 +1,3 @@
-; the return value of this routine dictates if we should wait for nmi or not right after
-; initGameBackground               gms: 1 acc: 0 - ne
-; initGameState                    gms: 2 acc: 4/0 - ne
-; updateCountersAndNonPlayerState  gms: 3 acc: 0/1 - ne
-; handleGameOver                   gms: 4 acc: eq (set to $9) if gameOver, $1 otherwise (ne)
-; updatePlayer1                    gms: 5 acc: $FF - ne
-; next                             gms: 6 acc: $1 ne
-; checkForResetKeyCombo            gms: 7 acc: 0 or heldButtons - eq if holding down, left and right
-; handlePause                      gms: 8 acc: 0/3 - ne
-; vblankThenRunState2              gms: 2 acc eq (set to $2)
-
 branchOnGameModeState:
         lda #0
         sta mainLoopWait
